902bdb4300f814276690932af98ecd3bc8357e66
Jayson Nabor <jayson@blockrenovation.com>
2022-09-27 14:50:28 -0400

    updates

--- Assets/Tutorials/Scripts/Editor/LightControl.cs
+++ Assets/Tutorials/Scripts/Editor/LightControl.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Unity.InteractiveTutorials;
+// using Unity.InteractiveTutorials;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -12,29 +12,7 @@ namespace Unity.Autoshowroom.Tutorials
 
         public static void RegisterActionToUnloadLightScene()
         {
-            Action actionForunloadingLight = () =>
-            {
-                UnityEditor.SceneManagement.EditorSceneManager.CloseScene(UnityEditor.SceneManagement.EditorSceneManager.GetSceneAt(1), true);
-            };
 
-            try
-            {
-                if (UnityEditor.SceneManagement.EditorSceneManager.sceneCount > 1)
-                {
-                    targetButton = EditorWindow.GetWindow<TutorialWindow>().rootVisualElement.Q<Button>("NextButton");
-
-                    if (targetButton != null)
-                    {
-                        targetButton.clicked -= actionForunloadingLight;
-                        targetButton.clicked += actionForunloadingLight;
-                    }
-                }
-            }
-            catch
-            {
-                Debug.LogError("Faile to register an action for unloading Scene.");
-                return;
-            }
         }
 
     }
